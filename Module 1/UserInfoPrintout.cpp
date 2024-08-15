2e605dc83d71551bde6e10e7fb9d366ac74d32bd
Charles Coyne <charles.s.coyne@gmail.com>
2024-08-15 15:06:12 -0400

    adjusted printout for legibility

--- Module 1/UserInfoPrintout.cpp
+++ Module 1/UserInfoPrintout.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 int main() {
     // Variables to store person's info
@@ -18,8 +19,8 @@ int main() {
 
     // Print out info
     std::cout << "Person's Information:" << std::endl;
-    std::cout << "Name:" << firstName << " " << lastName << std::endl;
-    std::cout << "Street Address:" << streetAddress << std::endl;
-    std::cout << "City:" << city << std::endl;
-    std::cout << "Zip Code:" << zipCode << std::endl;
+    std::cout << std::left << std::setw(16) << "Name:" << firstName << " " << lastName << std::endl;
+    std::cout << std::left << std::setw(16) << "Street Address:" << streetAddress << std::endl;
+    std::cout << std::left << std::setw(16) << "City:" << city << std::endl;
+    std::cout << std::left << std::setw(16) << "Zip Code:" << zipCode << std::endl;
 }
