d427e51915c29b84131680f1adfe991e31c31dae
Charles Coyne <charles.s.coyne@gmail.com>
2024-08-15 15:17:00 -0400

    second part files corrected and ran

--- Module 1/CSC450_CT1_mod1-1.cpp
+++ Module 1/CSC450_CT1_mod1-1.cpp
@@ -1,24 +1,21 @@
-/* Simple Program with a few Errors for Correction
+/* 
+   Simple Program with a few Errors for Correction
    Please be sure to correct all outlined errors.
-
+*/
 
 #include<iostream>
-#include<conio.h>
 
-Standard namespace declaration
+// Standard namespace declaration
 using namespace std;
 
-Main Function
+// Main Function
 int main()
 {
+   //Standard Ouput Statement
+   cout << "Welcome to this C++ Program" << endl;
 
-     //Standard Ouput Statement
-     cout<<"Welcome to this C++ Program"<<endl;
+   cout << "I have corrected all errors for this program." << endl;
 
-     cout<<I have corrected all errors for this program. <<endl;
-
-     // Wait For Output Screen
-
-     //Main Function return Statement
-
+   //Main Function return Statement
+   return 0;
 }
--- Module 1/CSC450_CT1_mod1-2.cpp
+++ Module 1/CSC450_CT1_mod1-2.cpp
@@ -1,27 +1,20 @@
-
-
 #include<iostream>
-#include<conio.h>
 
-Standard namespace declaration
+// Standard namespace declaration
 using namespace std;
 
-Main Function
+// Main Function
 int main()
 {
+     double myMoney = 1000.50;  // This should be printed out
 
-     double myMoney = 1000.50;  //this should be printed out
+     // Standard Ouput Statement
+     cout << "Please be sure to correct all syntax errors in this program" << endl;
 
+     cout << "I have corrected all errors for this program." << endl;
 
-     Standard Ouput Statement
-     cout"Please be sure to correct all syntax errors in this program"<<endl;
+     cout << "The total amount of money available is = " << myMoney << endl;
 
-     cout<<"I have corrected all errors for this program. <<endl;
-
-     cout<<" The total amount of money available is = "<<   <<endl;
-
-     // Wait For Output Screen
-
-     Main Function return Statement
+     // Main Function return Statement
      return 0;
 }
