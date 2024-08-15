284f3bcdd57784724595c3071b7b7647b9a98f3f
Charles Coyne <charles.s.coyne@gmail.com>
2024-08-15 14:57:53 -0400

    initialize variables

---
+++ Module 1/UserInfoPrintout.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    // Variables to store person's info
+    std::string firstName;
+    std::string lastName;
+    std::string streetAddress;
+    std::string city;
+    std::string zipCode;
+}
