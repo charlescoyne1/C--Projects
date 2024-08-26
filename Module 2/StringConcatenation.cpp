196bd961aca0b7550d1aca943b73d8197b705a46
Charles Coyne <charles.s.coyne@gmail.com>
2024-08-25 20:17:35 -0400

    setup main loop

--- Module 2/StringConcatenation.cpp
+++ Module 2/StringConcatenation.cpp
@@ -6,6 +6,11 @@ int main()
     // Variables for string concatenation
     std::string firstString, secondString, combinedString;
 
+    // Main function loop for 3 concatenations
+    for (int i = 0; i < 3; ++i)
+    {
+        
+    }
 
     return 0;
 }
