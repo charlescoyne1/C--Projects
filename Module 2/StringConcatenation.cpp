26ad9ff0573242fe049b762593e48dc365152905
Charles Coyne <charles.s.coyne@gmail.com>
2024-08-25 20:22:15 -0400

    finish up main function to read two inputs and output concated string

--- Module 2/StringConcatenation.cpp
+++ Module 2/StringConcatenation.cpp
@@ -9,7 +9,16 @@ int main()
     // Main function loop for 3 concatenations
     for (int i = 0; i < 3; ++i)
     {
-        
+        std::cout << "Enter the first string (" << i + 1 << "/3): ";
+        getline(std::cin, firstString); // getline allows use of entire line including spaces
+
+        std::cout << "Enter the second string (" << i + 1 << "/3: ";
+        getline(std::cin, secondString);
+
+        combinedString = firstString + secondString;
+
+        // Print the combined string
+        std::cout << "The concatenated string is: " << combinedString << std::endl;
     }
 
     return 0;
