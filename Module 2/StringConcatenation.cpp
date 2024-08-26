93fb2ebc02cf4ef610410a8db1714e24cf1bb792
Charles Coyne <charles.s.coyne@gmail.com>
2024-08-25 20:27:44 -0400

    refined with initial info on program and seperation line inbetween 3 total inputs.

--- Module 2/StringConcatenation.cpp
+++ Module 2/StringConcatenation.cpp
@@ -6,6 +6,9 @@ int main()
     // Variables for string concatenation
     std::string firstString, secondString, combinedString;
 
+    // Preamble about the program
+    std::cout << "This program takes 2 text inputs and combines them 3 times. When prompted please enter any text including spaces to then have it combined together." << std::endl;
+
     // Main function loop for 3 concatenations
     for (int i = 0; i < 3; ++i)
     {
@@ -19,6 +22,7 @@ int main()
 
         // Print the combined string
         std::cout << "The concatenated string is: " << combinedString << std::endl;
+        std::cout << "----------------------------" << std::endl;
     }
 
     return 0;
