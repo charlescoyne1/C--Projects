6cf0ecaed714cc57c42e0e77ed0bc03b46748522
Charles Coyne <charles.s.coyne@gmail.com>
2024-09-01 21:07:56 -0400

    start of program

--- Module 3/DynamicIntegerMemory.cpp
+++ Module 3/DynamicIntegerMemory.cpp
@@ -1,4 +1,13 @@
+#include <iostream>
+using namespace std;
+
 int main()
 {
-    
+    // Int Variables
+    int num1, num2, num3;
+
+    // Initial program info prompt
+    cout << "This program will take 3 integer inputs then outputs the integer as well as memory pointer value.";
+
+    // Prompt for user inputs
 }
