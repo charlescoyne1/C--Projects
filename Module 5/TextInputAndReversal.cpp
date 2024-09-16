ca48e40f442dc03d37972939ad193379a3282251
Charles Coyne <charles.s.coyne@gmail.com>
2024-09-15 21:37:56 -0400

    added file retrieval, reversal file and user input

--- Module 5/TextInputAndReversal.cpp
+++ Module 5/TextInputAndReversal.cpp
@@ -1,6 +1,15 @@
+#include <iostream>
+#include <string>
+
 int main()
 {
-    std::string filename = "/";
+    std::string filename = "/CSC450_CT5_mod5.txt";
+    std::string reversedFile = "/CSC450_CT5_mod5_reversed.txt";
+    std::string userInput;
+
+    // Get user input
+    std::cout << "Enter text to input to the file. (Will be appended to any existing text): ";
+    std::getline(std::cin, userInput);
 
     return 0;
 }
