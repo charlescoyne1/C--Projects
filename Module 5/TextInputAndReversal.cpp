7c62369fae3c06646f007d69c491208c6b99d263
Charles Coyne <charles.s.coyne@gmail.com>
2024-09-15 21:50:03 -0400

    created the appending text method

--- Module 5/TextInputAndReversal.cpp
+++ Module 5/TextInputAndReversal.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <algorithm>
+
+void appendToText(const std::string &filename, const std::string &input) 
+{
+    std::ofstream file;
+    // Open the txt file to append on
+    file.open(filename, std::ios::app);
+    if(file..is_open()) 
+    {
+        file << input << std::endl;
+        file.close();
+        std::cout << "Input suuccessfully appended to txt file " << filename << std::endl;
+    }
+    else
+    {
+        std::cerr << "Unable to open file, check to see if file is not already opened, then close and rerun." << std::endl;
+    }
+}
 
 int main()
 {
@@ -10,6 +29,8 @@ int main()
     // Get user input
     std::cout << "Enter text to input to the file. (Will be appended to any existing text): ";
     std::getline(std::cin, userInput);
+
+
 
     return 0;
 }
