d3c0ebd3f3fca755875c292794f1ced05ac393d5
Charles Coyne <charles.s.coyne@gmail.com>
2024-09-29 20:32:30 -0400

    updating includes and first method logic

--- Module 7/ThreadConcurrency.cpp
+++ Module 7/ThreadConcurrency.cpp
@@ -1,11 +1,21 @@
+#include <iostream>
+#include <thread>
+#include <mutex>
+
+std::mutex mtx; 
+
 void countUp()
 {
-
+    for (int i = 1; i <= 20; ++i)
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        std::cout << "Counting up: " << i << std::endl;
+    }
 }
 
 void countDown()
 {
-    
+
 }
 
 int main()
