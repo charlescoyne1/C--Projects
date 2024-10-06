a4eed0d225fc944843ad2afba66d503d687cecb5
Charles Coyne <charles.s.coyne@gmail.com>
2024-10-06 19:58:37 -0400

    created main function sans-counting

--- Module 8/JavaThreadConcurrency.java
+++ Module 8/JavaThreadConcurrency.java
@@ -1,5 +1,19 @@
 public class JavaThreadConcurrency {
     public static void main(String[] args) {
-        
+        // Concurrent thread creation
+        Thread thread1 = new Thread();
+        Thread thread2 = new Thread();
+
+        // Concurrent thread start
+        thread1.start();
+        thread2.start();
+
+        // End after counting completed
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (Exception e) {
+            System.out.println("Thread interrupted: " + e.getMessage());
+        }
     }
 }
