3a4cad46838e234ee0eced9ef0c77902011c9f08
Charles Coyne <charles.s.coyne@gmail.com>
2024-10-06 20:02:30 -0400

    count up and down functions added

--- Module 8/JavaThreadConcurrency.java
+++ Module 8/JavaThreadConcurrency.java
@@ -1,4 +1,26 @@
 public class JavaThreadConcurrency {
+
+    // Object for concurrency
+    private static final Object lock = new Object();
+
+    // Function counting up
+    public static void countUp() {
+        for (int i = 0; i <= 20; i++) {
+            synchronized (lock) {
+                System.out.println("Counting up: " + i);
+            }
+        }
+    }
+
+    // Function counting down
+    public static void countDown() {
+        for (int i = 20; i >= 0; i--) {
+            synchronized (lock) {
+                System.out.println("Counting down: " + i);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         // Concurrent thread creation
         Thread thread1 = new Thread();
